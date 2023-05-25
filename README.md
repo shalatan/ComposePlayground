b95b8e6b198872976f9501f43eca5c1e60710881
shashank-dropshop <shashank.singh@dropshop.network>
2023-05-25 13:48:11 +0530

    TopicsApp [lazyXGrid]

---
+++ README.md
@@ -0,0 +1,5 @@
+<h1 align="center">Compose Playground</h1>
+
+-
+- AffirmationsApp
+- TopicsApp
---
+++ app/src/main/java/com/example/myapplication/unit3/TopicsApp.kt
@@ -0,0 +1,152 @@
+package com.example.myapplication.unit3
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.annotation.DrawableRes
+import androidx.annotation.StringRes
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.GridItemSpan
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.lazy.grid.items
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Card
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.myapplication.R
+import com.example.myapplication.ui.theme.MyApplicationTheme
+
+data class Topic(
+    @StringRes val name: Int,
+    val point: Int,
+    @DrawableRes val image: Int
+)
+
+object TopicDataSource {
+    val topics = listOf(
+        Topic(R.string.architecture, 58, R.drawable.architecture),
+        Topic(R.string.crafts, 121, R.drawable.crafts),
+        Topic(R.string.business, 78, R.drawable.business),
+        Topic(R.string.culinary, 118, R.drawable.culinary),
+        Topic(R.string.design, 423, R.drawable.design),
+        Topic(R.string.fashion, 92, R.drawable.fashion),
+        Topic(R.string.film, 165, R.drawable.film),
+        Topic(R.string.gaming, 164, R.drawable.gaming),
+        Topic(R.string.drawing, 326, R.drawable.drawing),
+        Topic(R.string.lifestyle, 305, R.drawable.lifestyle),
+        Topic(R.string.music, 212, R.drawable.music),
+        Topic(R.string.painting, 172, R.drawable.painting),
+        Topic(R.string.photography, 321, R.drawable.photography),
+        Topic(R.string.tech, 118, R.drawable.tech)
+    )
+}
+
+class TopicsApp : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            MyApplicationTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    TopicsScreen()
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun TopicsScreen() {
+    TopicsRecyclerView(
+        topics = TopicDataSource.topics,
+        modifier = Modifier.padding(8.dp)
+    )
+}
+
+@Composable
+fun TopicsRecyclerView(topics: List<Topic>, modifier: Modifier = Modifier) {
+    GridItemSpan(2)
+    LazyVerticalGrid(
+        columns = GridCells.Fixed(2),
+        verticalArrangement = Arrangement.spacedBy(8.dp),
+        horizontalArrangement = Arrangement.spacedBy(8.dp),
+        modifier = modifier
+    ) {
+        items(topics) { topic ->
+            TopicsCard(topic = topic, modifier = Modifier)
+        }
+    }
+}
+
+@Composable
+fun TopicsCard(topic: Topic, modifier: Modifier = Modifier) {
+    Card(
+        shape = RoundedCornerShape(16.dp),
+        modifier = modifier
+    ) {
+        Row(modifier = Modifier.fillMaxWidth()) {
+            Image(
+                painter = painterResource(id = topic.image),
+                contentDescription = stringResource(
+                    id = topic.name
+                ),
+                modifier = Modifier.size(68.dp)
+            )
+            Column(
+                modifier = Modifier.padding(
+                    top = 16.dp,
+                    start = 16.dp,
+                    end = 16.dp,
+                    bottom = 4.dp
+                )
+            ) {
+                Text(
+                    text = stringResource(id = topic.name),
+                    style = MaterialTheme.typography.bodyMedium
+                )
+                Row(
+                    modifier = Modifier.padding(top = 8.dp),
+                    verticalAlignment = Alignment.CenterVertically
+                ) {
+                    Icon(
+                        painter = painterResource(id = R.drawable.ic_grain),
+                        contentDescription = null
+                    )
+                    Text(
+                        text = topic.point.toString(),
+                        style = MaterialTheme.typography.labelMedium,
+                        modifier = Modifier.padding(start = 8.dp)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    MyApplicationTheme {
+        TopicsScreen()
+    }
+}
